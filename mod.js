05b8edc6da3c0b92ca95d746d0c1b4823bb281db
Viktor Berke <github.bviktor@outlook.com>
2025-11-20 00:22:10 +0100

    Light beam for gems Fixes #15

--- mod.js
+++ mod.js
@@ -1,218 +1,5 @@
 const langJsonDir = "local\\lng\\strings";
 
-function addLightBeam(jsonFile)
-{
-	// Read the JSON file
-	var itemJson = D2RMM.readJson(jsonFile);
-
-	// Extend the JSON with the light beam stuff
-	itemJson.dependencies.particles.push(lightBeamParticleJson);
-
-	for (var i in lightBeamEntityJson)
-	{
-		itemJson.entities.push(lightBeamEntityJson[i]);
-	}
-
-	// Write the JSON file
-	D2RMM.writeJson(jsonFile, itemJson);
-}
-
-function highlightGems(jsonFile)
-{
-	const jsonFilePath = langJsonDir + "\\" + jsonFile + ".json";
-
-	// Parse the JSON holding the items
-	const itemJson = D2RMM.readJson(jsonFilePath);
-
-	itemJson.forEach((item) =>
-	{
-		let newName = null;
-		let gemPrefix = null;
-		let gemColor = null;
-		let itemFirstWord = item.enUS.split(" ")[0];
-
-		switch (itemFirstWord)
-		{
-			case "Chipped":
-				gemPrefix = "-";
-				break;
-			case "Flawed":
-				gemPrefix = "+";
-				break;
-			case "Flawless":
-				gemPrefix = "@";
-				break;
-			case "Perfect":
-				gemPrefix = "O";
-				break;
-			default: // "Normal" gems don't have a prefix
-				gemPrefix = "x";
-		}
-
-		switch (item.Key)
-		{
-			case "gcv": // Chipped Amethyst
-			case "gfv": // Flawed Amethyst
-			case "gsv": // Amethyst
-			case "gzv": // Flawless Amethyst
-			case "gpv": // Perfect Amethyst
-				gemColor = `ÿc;`
-				break;
-			case "gcw": // Chipped Diamond
-			case "gfw": // Flawed Diamond
-			case "gsw": // Diamond
-			case "glw": // Flawless Diamond
-			case "gpw": // Perfect Diamond
-				gemColor = `ÿcF`;
-				break;
-			case "gcg": // Chipped Emerald
-			case "gfg": // Flawed Emerald
-			case "gsg": // Emerald
-			case "glg": // Flawless Emerald
-			case "gpg": // Perfect Emerald
-				gemColor = `ÿcQ`;
-				break;
-			case "gcr": // Chipped Ruby
-			case "gfr": // Flawed Ruby
-			case "gsr": // Ruby
-			case "glr": // Flawless Ruby
-			case "gpr": // Perfect Ruby
-				gemColor = `ÿcU`;
-				break;
-			case "gcb": // Chipped Sapphire
-			case "gfb": // Flawed Sapphire
-			case "gsb": // Sapphire
-			case "glb": // Flawless Sapphire
-			case "gpb": // Perfect Sapphire
-				gemColor = `ÿcN`;
-				break;
-			case "skc": // Chipped Skull
-			case "skf": // Flawed Skull
-			case "sku": // Skull
-			case "skl": // Flawless Skull
-			case "skz": // Perfect Skull
-				gemColor = `ÿc5`;
-				break;
-			case "gcy": // Chipped Topaz
-			case "gfy": // Flawed Topaz
-			case "gsy": // Topaz
-			case "gly": // Flawless Topaz
-			case "gpy": // Perfect Topaz
-				gemColor = `ÿc9`;
-				break;
-		}
-
-		// Now gemColor is only non-null if it's a gem
-		// item-nameaffixes has duplicate entries, e.g. Key = 'Emerald' which is an affix, not an item
-		// However, the length of the key of items is always 3, so check for that
-		if (gemColor != null && item.Key.length == 3)
-		{
-			newName = `ÿc7[` + gemColor + gemPrefix + `ÿc7] ÿc0` + item.enUS;
-
-			// Update all localizations
-			for (const key in item)
-			{
-				if (key !== 'id' && key !== 'Key')
-				{
-					item[key] = newName;
-				}
-			}
-		}
-	});
-
-	D2RMM.writeJson(jsonFilePath, itemJson);
-}
-
-// More vivid gem names
-if (Object.hasOwn(config, "misc_gemhl") && config["misc_gemhl"])
-{
-	highlightGems("item-names");
-	// For whatever braindamaged reason, the normal Sapphire, Emerald, Ruby and Diamond
-	// definitions are not in item-names, but in item-nameaffixes instead
-	highlightGems("item-nameaffixes");
-}
-
-//Change items in item-names.json (potions, scrolls, charms, jewels)
-const itemNamesFilename = 'local\\lng\\strings\\item-names.json';
-const itemNames = D2RMM.readJson(itemNamesFilename);
-
-itemNames.forEach((item) =>
-{
-	const itemtype = item.Key;
-	let newName = null;
-
-	// Highlight base item if set to be highlighted, but first check if it actually exists in the config
-	if (Object.hasOwn(config, "bth_" + itemtype) && config["bth_" + itemtype])
-	{
-		newName = item.enUS + ` ÿc;*ÿc2*ÿc1* ÿc@Base ÿc;*ÿc2*ÿc1*`;
-	}
-
-	switch (itemtype)
-	{
-		case "rvl": // Full Rejuvenation Potion
-			newName = `Full ÿc;Rejuvenationÿc0 Potion`;
-			break;
-		case "cm1": // Small Charm
-			newName = `Small Charm ÿcO•`;
-			break;
-		case "cm2": // Large Charm
-			newName = `Large Charm ÿcO••`;
-			break;
-		case "cm3": // Grand Charm
-			newName = `Grand Charm ÿcO•••`;
-			break;
-		case "jew": // Jewel
-			newName = `• Jewel •ÿcN`;
-			break;
-	}
-
-	if (newName != null)
-	{
-		// update all localizations
-		for (const key in item)
-		{
-			if (key !== 'id' && key !== 'Key')
-			{
-				item[key] = newName;
-			}
-		}
-	}
-});
-D2RMM.writeJson(itemNamesFilename, itemNames);
-
-// Change gold item label on the ground
-if (Object.hasOwn(config, "misc_gold") && config["misc_gold"])
-{
-	const itemNameAffixesFilename = 'local\\lng\\strings\\item-nameaffixes.json';
-	const itemNameAffixes = D2RMM.readJson(itemNameAffixesFilename);
-
-	itemNameAffixes.forEach((item) =>
-	{
-		const itemtype = item.Key;
-		let newName = null;
-
-		// Gold
-		if (itemtype === 'gld')
-		{
-			newName = ``;
-		}
-
-		if (newName != null)
-		{
-			// update all localizations
-			for (const key in item)
-			{
-				if (key !== 'id' && key !== 'Key')
-				{
-					item[key] = newName;
-				}
-			}
-		}
-	});
-
-	D2RMM.writeJson(itemNameAffixesFilename, itemNameAffixes);
-}
-
 const lightBeamParticleJson = {
 	"path": "data/hd/vfx/particles/overlays/object/horadric_light/fx_horadric_light.particles"
 }
@@ -362,6 +149,230 @@ const lightBeamEntityJson = [
 		"hardKillOnDestroy": false
 	}]
 }]
+
+function addLightBeam(jsonFile)
+{
+	// Read the JSON file
+	var itemJson = D2RMM.readJson(jsonFile);
+
+	// Extend the JSON with the light beam stuff
+	itemJson.dependencies.particles.push(lightBeamParticleJson);
+
+	for (var i in lightBeamEntityJson)
+	{
+		itemJson.entities.push(lightBeamEntityJson[i]);
+	}
+
+	// Write the JSON file
+	D2RMM.writeJson(jsonFile, itemJson);
+}
+
+function highlightGems(jsonFile)
+{
+	const jsonFilePath = langJsonDir + "\\" + jsonFile + ".json";
+	const gemsDir = "hd\\items\\misc\\gem";
+
+	// Parse the JSON holding the items
+	const itemJson = D2RMM.readJson(jsonFilePath);
+
+	itemJson.forEach((item) =>
+	{
+		let newName = null;
+		let gemPrefix = null;
+		let gemColor = null;
+		let itemFirstWord = item.enUS.split(" ")[0];
+
+		switch (itemFirstWord)
+		{
+			case "Chipped":
+				gemPrefix = "-";
+				break;
+			case "Flawed":
+				gemPrefix = "+";
+				break;
+			case "Flawless":
+				gemPrefix = "@";
+				break;
+			case "Perfect":
+				gemPrefix = "O";
+				break;
+			default: // "Normal" gems don't have a prefix
+				gemPrefix = "x";
+		}
+
+		switch (item.Key)
+		{
+			case "gcv": // Chipped Amethyst
+			case "gfv": // Flawed Amethyst
+			case "gsv": // Amethyst
+			case "gzv": // Flawless Amethyst
+			case "gpv": // Perfect Amethyst
+				gemColor = `ÿc;`
+				break;
+			case "gcw": // Chipped Diamond
+			case "gfw": // Flawed Diamond
+			case "gsw": // Diamond
+			case "glw": // Flawless Diamond
+			case "gpw": // Perfect Diamond
+				gemColor = `ÿcF`;
+				break;
+			case "gcg": // Chipped Emerald
+			case "gfg": // Flawed Emerald
+			case "gsg": // Emerald
+			case "glg": // Flawless Emerald
+			case "gpg": // Perfect Emerald
+				gemColor = `ÿcQ`;
+				break;
+			case "gcr": // Chipped Ruby
+			case "gfr": // Flawed Ruby
+			case "gsr": // Ruby
+			case "glr": // Flawless Ruby
+			case "gpr": // Perfect Ruby
+				gemColor = `ÿcU`;
+				break;
+			case "gcb": // Chipped Sapphire
+			case "gfb": // Flawed Sapphire
+			case "gsb": // Sapphire
+			case "glb": // Flawless Sapphire
+			case "gpb": // Perfect Sapphire
+				gemColor = `ÿcN`;
+				break;
+			case "skc": // Chipped Skull
+			case "skf": // Flawed Skull
+			case "sku": // Skull
+			case "skl": // Flawless Skull
+			case "skz": // Perfect Skull
+				gemColor = `ÿc5`;
+				break;
+			case "gcy": // Chipped Topaz
+			case "gfy": // Flawed Topaz
+			case "gsy": // Topaz
+			case "gly": // Flawless Topaz
+			case "gpy": // Perfect Topaz
+				gemColor = `ÿc9`;
+				break;
+		}
+
+		// Now gemColor is only non-null if it's a gem
+		// item-nameaffixes has duplicate entries, e.g. Key = 'Emerald' which is an affix, not an item
+		// However, the length of the key of items is always 3, so check for that
+		if (gemColor != null && item.Key.length == 3)
+		{
+			// Make sure this is done *before* renaming the item, otherwise the path will be corrupted
+			if (Object.hasOwn(config, "lbg_" + item.Key) && config["lbg_" + item.Key])
+			{
+				// Get the string "Flawed Amethyst", replace space with underscore, and convert to lowercase
+				const gemFile = gemsDir + "\\" + item.enUS.replace(" ", "_").toLowerCase() + ".json";
+
+				// Apply the light beam
+				addLightBeam(gemFile);
+			}
+
+			newName = `ÿc7[` + gemColor + gemPrefix + `ÿc7] ÿc0` + item.enUS;
+
+			// Update all localizations
+			for (const key in item)
+			{
+				if (key !== 'id' && key !== 'Key')
+				{
+					item[key] = newName;
+				}
+			}
+		}
+	});
+
+	D2RMM.writeJson(jsonFilePath, itemJson);
+}
+
+// More vivid gem names
+if (Object.hasOwn(config, "misc_gemhl") && config["misc_gemhl"])
+{
+	highlightGems("item-names");
+	// For whatever braindamaged reason, the normal Sapphire, Emerald, Ruby and Diamond
+	// definitions are not in item-names, but in item-nameaffixes instead
+	highlightGems("item-nameaffixes");
+}
+
+//Change items in item-names.json (potions, scrolls, charms, jewels)
+const itemNamesFilename = 'local\\lng\\strings\\item-names.json';
+const itemNames = D2RMM.readJson(itemNamesFilename);
+
+itemNames.forEach((item) =>
+{
+	const itemtype = item.Key;
+	let newName = null;
+
+	// Highlight base item if set to be highlighted, but first check if it actually exists in the config
+	if (Object.hasOwn(config, "bth_" + itemtype) && config["bth_" + itemtype])
+	{
+		newName = item.enUS + ` ÿc;*ÿc2*ÿc1* ÿc@Base ÿc;*ÿc2*ÿc1*`;
+	}
+
+	switch (itemtype)
+	{
+		case "rvl": // Full Rejuvenation Potion
+			newName = `Full ÿc;Rejuvenationÿc0 Potion`;
+			break;
+		case "cm1": // Small Charm
+			newName = `Small Charm ÿcO•`;
+			break;
+		case "cm2": // Large Charm
+			newName = `Large Charm ÿcO••`;
+			break;
+		case "cm3": // Grand Charm
+			newName = `Grand Charm ÿcO•••`;
+			break;
+		case "jew": // Jewel
+			newName = `• Jewel •ÿcN`;
+			break;
+	}
+
+	if (newName != null)
+	{
+		// update all localizations
+		for (const key in item)
+		{
+			if (key !== 'id' && key !== 'Key')
+			{
+				item[key] = newName;
+			}
+		}
+	}
+});
+D2RMM.writeJson(itemNamesFilename, itemNames);
+
+// Change gold item label on the ground
+if (Object.hasOwn(config, "misc_gold") && config["misc_gold"])
+{
+	const itemNameAffixesFilename = 'local\\lng\\strings\\item-nameaffixes.json';
+	const itemNameAffixes = D2RMM.readJson(itemNameAffixesFilename);
+
+	itemNameAffixes.forEach((item) =>
+	{
+		const itemtype = item.Key;
+		let newName = null;
+
+		// Gold
+		if (itemtype === 'gld')
+		{
+			newName = ``;
+		}
+
+		if (newName != null)
+		{
+			// update all localizations
+			for (const key in item)
+			{
+				if (key !== 'id' && key !== 'Key')
+				{
+					item[key] = newName;
+				}
+			}
+		}
+	});
+
+	D2RMM.writeJson(itemNameAffixesFilename, itemNameAffixes);
+}
 
 // Directory containing rune definitions
 const runesDir = "hd\\items\\misc\\rune";
